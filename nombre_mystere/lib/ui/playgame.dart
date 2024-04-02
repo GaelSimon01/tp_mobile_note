0296ac41debd42b67c1148abac8f44db44853dcd
w136226 <steven.marmion@worldline.com>
2024-04-02 22:17:31 +0200

    Ajout de l'entrée de valeur par la touche ENTREE du clvaier

--- nombre_mystere/lib/ui/playgame.dart
+++ nombre_mystere/lib/ui/playgame.dart
@@ -9,7 +9,7 @@ class GamePage extends StatefulWidget {
   final int niveau;
   final String player;
 
-  const GamePage({super.key, required this.niveau, required this.player});
+  const GamePage({Key? key, required this.niveau, required this.player}) : super(key: key);
 
   @override
   _GamePageState createState() => _GamePageState();
@@ -58,6 +58,109 @@ class _GamePageState extends State<GamePage> {
     _maxTentatives = 0;
   }
 
+  void _validateNumber() {
+    int guessedNumber = int.tryParse(_numberController.text) ?? 0;
+    if(_numberController.text.isEmpty){
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Erreur'),
+            content: const Text('Veuillez entrer un nombre'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+    else {
+      setState(() {
+        _remainingAttempts--;
+        _dernierNombreTestes = guessedNumber;
+        _numberController.clear();
+      });
+      if (guessedNumber == _randomNumber) {
+        _estGagne = true;
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text("Félicitations !"),
+            content: Text("Vous avez trouvé le nombre $_randomNumber !"),
+            actions: [
+              ElevatedButton(
+                onPressed: () async {
+                  resetStatusGame();
+                  await _generateRandomNumber();
+                  Navigator.pop(context);
+                },
+                child: const Text("Recommencer"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await RequestHelper.insertGame(_playerActuel[0]['id'], widget.niveau, _maxTentatives-_remainingAttempts, _estGagne);
+                  print(await RequestHelper.getPlayerGames(_playerActuel[0]['id']));
+                  context.go('/home/pre-game');
+                },
+                child: const Text("Enregistrer la partie"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await RequestHelper.insertGame(_playerActuel[0]['id'], widget.niveau, _maxTentatives-_remainingAttempts, _estGagne);
+                  print(await RequestHelper.getPlayerGames(_playerActuel[0]['id']));
+                  context.go('/home/pre-game?niveau=${widget.niveau+1}&player=${_playerActuel[0]['nom']}');
+                },
+                child: const Text("Niveau suivant"),
+              ),
+            ],
+          ),
+        );
+      } else {
+        if (guessedNumber < _randomNumber) {
+          setState(() {
+            _indication = "Plus haut";
+          });
+        } else if (guessedNumber > _randomNumber) {
+          setState(() {
+            _indication = "Plus bas";
+          });
+        }
+        if (_remainingAttempts == 0) {
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: const Text("Désolé !"),
+              content: Text("Vous avez épuisé tous vos essais. Le nombre était $_randomNumber."),
+              actions: [
+                ElevatedButton(
+                  onPressed: () async {
+                    resetStatusGame();
+                    await _generateRandomNumber();
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Recommencer"),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await RequestHelper.insertGame(_playerActuel[0]['id'], widget.niveau, _maxTentatives-_remainingAttempts, _estGagne);
+                    print(await RequestHelper.getPlayerGames(_playerActuel[0]['id']));
+                    context.go('/home/pre-game');
+                  },
+                  child: const Text("Enregistrer la partie"),
+                ),
+              ],
+            ),
+          );
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,125 +179,25 @@ class _GamePageState extends State<GamePage> {
             const Padding(padding: EdgeInsets.all(16.0)),
             Center(
               child: Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                  child: TextField(
-                    controller: _numberController,
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly, RangeTextInputFormatter( min: 0 ,max: _plageMax)],
-                    decoration: InputDecoration(
-                      hintText: "Entrez un nombre entre 0 et $_plageMax",
-                    ),
+                padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                child: TextField(
+                  controller: _numberController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly, RangeTextInputFormatter( min: 0 ,max: _plageMax)],
+                  decoration: InputDecoration(
+                    hintText: "Entrez un nombre entre 0 et $_plageMax",
                   ),
+                  onSubmitted: (_) {
+                    _validateNumber();
+                  },
+                ),
               ),
             ),
             const Padding(padding: EdgeInsets.all(16.0)),
             Text("Dernier nombre testé : $_dernierNombreTestes"),
             const Padding(padding: EdgeInsets.all(20.0)),
             ElevatedButton(
-              onPressed: () {
-                int guessedNumber = int.tryParse(_numberController.text) ?? 0;
-                if(_numberController.text==""){
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('Erreur'),
-                      content: const Text('Veuillez entrer un nombre'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('OK'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-                }
-                else if (guessedNumber == _randomNumber) {
-                  _estGagne = true;
-                  showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                      title: const Text("Félicitations !"),
-                      content: Text("Vous avez trouvé le nombre $_randomNumber !"),
-                      actions: [
-                        ElevatedButton(
-                          onPressed: () async {
-                            resetStatusGame();
-                            await _generateRandomNumber();
-                            _numberController.clear();
-                            Navigator.pop(context);
-                          },
-                          child: const Text("Recommencer"),
-                        ),
-                        ElevatedButton(
-                            onPressed: () async {
-                              await RequestHelper.insertGame(_playerActuel[0]['id'], widget.niveau, _maxTentatives-_remainingAttempts, _estGagne);
-                              print(await RequestHelper.getPlayerGames(_playerActuel[0]['id']));
-                              context.go('/home/pre-game');
-                            },
-                            child: const Text("Enregistrer la partie"),
-                          ),
-                        ElevatedButton(
-                            onPressed: () async {
-                              await RequestHelper.insertGame(_playerActuel[0]['id'], widget.niveau, _maxTentatives-_remainingAttempts, _estGagne);
-                              print(await RequestHelper.getPlayerGames(_playerActuel[0]['id']));
-                              context.go('/home/pre-game?niveau=${widget.niveau+1}&player=${_playerActuel[0]['nom']}');
-                            },
-                            child: const Text("Niveau suivant"),
-                          ),
-                      ],
-                    ),
-                  );
-                } else {
-                    setState(() {
-                      _remainingAttempts--;
-                      _dernierNombreTestes = int.tryParse(_numberController.text)!;
-                      _numberController.clear();
-                    });
-                    if (guessedNumber < _randomNumber) {
-                      setState(() {
-                        _indication = "Plus haut";
-                      });
-                    }
-                    else if (guessedNumber > _randomNumber) {
-                      setState(() {
-                        _indication = "Plus bas";
-                      });
-                    }
-                  if (_remainingAttempts == 0) {
-                    // Plus d'essais restants
-                    showDialog(
-                      context: context,
-                      builder: (_) => AlertDialog(
-                        title: const Text("Désolé !"),
-                        content: Text("Vous avez épuisé tous vos essais. Le nombre était $_randomNumber."),
-                        actions: [
-                          ElevatedButton(
-                            onPressed: () async {
-                              resetStatusGame();
-                              await _generateRandomNumber();
-                              _numberController.clear();
-                              Navigator.pop(context);
-                            },
-                            child: const Text("Recommencer"),
-                          ),
-                          ElevatedButton(
-                            onPressed: () async {
-                              await RequestHelper.insertGame(_playerActuel[0]['id'], widget.niveau, _maxTentatives-_remainingAttempts, _estGagne);
-                              print(await RequestHelper.getPlayerGames(_playerActuel[0]['id']));
-                              context.go('/home/pre-game');
-                            },
-                            child: const Text("Enregistrer la partie"),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                }
-              },
+              onPressed: _validateNumber,
               child: const Text("Valider"),
             ),
           ],
